79a63319e181966d9e9034720823b6d5f88dd442
dhruvsatyapanthi007 <dhruvsatyapanthi@gmail.com>
2023-05-07 00:45:57 -0400

    Update client.c

--- client.c
+++ client.c
@@ -3,11 +3,13 @@
 #include <sys/socket.h> //socket
 #include <arpa/inet.h>  //inet_addr
 #include <unistd.h>
+#include <time.h>
 
 struct queue
 {
 	char string[2000];
 	struct queue *next;
+    time_t start_time;
 };
 
 struct thread_args
@@ -18,6 +20,10 @@ struct thread_args
 
 static int file_number = 0;
 static int curr_file_number = 0;
+
+double server_avg_resp_time[3] = {0, 0, 0};
+int query_processed[3] = {0, 0, 0};
+int server_avg_server_utilization_time[3] = {0, 0, 0};
 
 struct queue* heads[3];
 struct queue* tails[3];
@@ -31,6 +37,8 @@ int load_balancer(){
 
 void enqueue(char *re, int i)
 {
+    time_t start_time;
+    time(&start_time);
 	if (heads[i] == NULL)
 	{
 		heads[i] = (struct queue *)malloc(sizeof(struct queue));
@@ -45,6 +53,7 @@ void enqueue(char *re, int i)
 		strcpy(tails[i]->string, re);
 		tails[i]->next = NULL;
 	}
+    tails[i]->start_time = start_time;
 }
 
 struct queue *dequeue(int i)
@@ -61,30 +70,59 @@ void *communicate_with_server(void *ta)
 	struct thread_args targ = *(struct thread_args *)ta;
     int socket = targ.socket;
     int idx = targ.idx;
+    int flag = 0;
 	while(1)
 	{
+        int break_flag = 0;
         if(heads[idx] != NULL)
         {
-            printf("%s\n", heads[idx]->string);
             if (send(socket, heads[idx]->string, strlen(heads[idx]->string), 0) < 0)
             {
                 puts("Send failed");
                 return;
             }
-            printf("%s\n", heads[idx]->string);
-            while(1)
+            time_t end_time;
+            while(!break_flag)
             {
                 if(recv(socket, buffer, 2000, 0) > 0)
                 {
-                    printf("%s\n", buffer);
-                    if(strcmp(buffer, "!@#$%^&*()") == 0)
+                    if(strchr(buffer, '#') != NULL)
                     {
-                        memset(buffer, 0, strlen(buffer));
-                        break;
+                        char* token = strtok(buffer, "#");
+                        while(token != NULL)
+                        {
+                            if(flag)
+                            {
+                                char* p;
+                                int server_time = atoi(token);
+                                printf("Inside flag condition\n");
+                                printf("The value is %d\n", server_time);
+                                printf("Token is %s\n", token);
+                                server_avg_server_utilization_time[idx] += server_time;
+                                flag = 0;
+                            }
+                            else if(strcmp(token, "serverUtilizationTime") == 0)
+                            {
+                                flag = 1;
+                            }
+                            else if(strcmp(token, "!@$%^&*()") == 0)
+                            {
+                                time(&end_time);
+                                query_processed[idx] += 1;
+                                memset(buffer, 0, strlen(buffer));
+                                break_flag = 1;
+                                break;
+                            }
+                            token = strtok(NULL, "#");
+                        }
                     }
                     memset(buffer, 0, strlen(buffer));
                 }
             }
+            double diff_t = difftime(end_time, heads[idx]->start_time);
+            printf("Time taken: %f\n", diff_t);
+            double server_time = server_avg_resp_time[idx] * (query_processed[idx] - 1);
+            server_avg_resp_time[idx] = (server_time + diff_t)/query_processed[idx];
             dequeue(idx);
         }
 	}
@@ -201,21 +239,36 @@ int main(int argc, char *argv[])
         printf("Thread created in Client for server 3\n");
     }
 
-    keep communicating with server
+    // keep communicating with server
     while (1)
     {
-        printf("Enter message :");
+        printf("Enter message or type exit to exit the program:");
         scanf("%s", message);
+
+        if(strcmp(message, "exit") == 0)
+        {
+            printf("Printing out the Load Balancer Stats:\n");
+            printf("Server average Response Time:\n");
+            double avg_resp_time = (server_avg_resp_time[0] + server_avg_resp_time[1] + server_avg_resp_time[2])/3.0;
+            printf("%f\n", server_avg_resp_time[0]);
+            printf("Server average Throughput:\n");
+            double avg_throughput = ((query_processed[0]+0.001)/server_avg_resp_time[0]) + (query_processed[1]+0.001/server_avg_resp_time[1]) + (query_processed[2]+0.001/server_avg_resp_time[2])/3.0;
+            printf("%f\n", avg_throughput);
+            printf("Server average Utilization:\n");
+            double avg_server_utilization = ((server_avg_server_utilization_time[0])/server_avg_resp_time[0] + (server_avg_server_utilization_time[1])/server_avg_resp_time[1] + (server_avg_server_utilization_time[2])/server_avg_resp_time[2])/3.0;
+            printf("%f\n", avg_server_utilization);
+            break;
+        }
 
         int idx = load_balancer();
         enqueue(message, idx);
+
     }
 
     rc = pthread_join(read_t[0], NULL);
     rc = pthread_join(read_t[1], NULL);
     rc = pthread_join(read_t[2], NULL);
-
-    printf("I am before close!");
+    
     close(sockets[0]);
     return 0;
 }
